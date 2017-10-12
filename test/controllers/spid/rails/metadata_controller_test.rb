da9bd6f04d08b885053590a7b13994394bae62e5
Alessandro Descovi <descovi@gmail.com>
2017-10-12 10:37:24 +0200

    test per metadata

--- test/controllers/spid/rails/metadata_controller_test.rb
+++ test/controllers/spid/rails/metadata_controller_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 module Spid::Rails
   class MetadataControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
-
-    # test "the truth" do
-    #   assert true
-    # end
+    
+    test "the truth" do
+      get metadata_path
+      assert_response :ok
+    end
   end
 end
