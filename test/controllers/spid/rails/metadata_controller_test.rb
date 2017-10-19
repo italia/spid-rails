85151b7800d9ab9f9e3d60933d324951bb98af1d
Jecko-o <tunedstoned@yahoo.it>
2017-10-19 15:34:57 +0200

    create separate integration metadata conformity test

--- test/controllers/spid/rails/metadata_controller_test.rb
+++ test/controllers/spid/rails/metadata_controller_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
-require 'open-uri'
 
 module Spid::Rails
 
@@ -7,63 +6,12 @@ module Spid::Rails
     include Engine.routes.url_helpers
 
     setup do
-      # TODO: rails server start should not be needed
-      metadata = open("http://localhost:3000/spid/metadata")
-      @metadata = Nokogiri::XML(metadata)
-      @namespaces = Spid::Rails::Metadata.xml_namespaces
-      @allowed_signature_algorithms = Spid::Rails::Certificate.signature_algorithms
-      @allowed_digest_algorithms = Spid::Rails::Certificate.digest_algorithms
+      @routes = Engine.routes
     end
 
     test "get metadata url" do
       get metadata_url
-      assert_response :ok
-    end
-
-    test "conformità EntityDescriptor" do
-      node = @metadata.at_xpath("//md:EntityDescriptor")
-      assert node.present?
-      assert node.attr('ID').present?
-      assert node.attr('entityID').present?
-    end
-
-    test "conformità KeyDescriptor" do
-      node = @metadata.at_xpath("//md:KeyDescriptor")
-      assert node.present?
-
-      signature_node = node.at_xpath("//ds:Signature", @namespaces)
-      assert signature_node.present?
-
-      certificate_node = node.at_xpath("//ds:X509Certificate", @namespaces)
-      assert certificate_node.present?
-    end
-
-    test 'conformità signature' do
-      signature_value_node = @metadata.at_xpath("//ds:Signature/ds:SignatureValue",
-                                            @namespaces)
-      signature = signature_value_node.text
-      assert signature.present?
-
-
-      signature_method_node = @metadata.at_xpath("//ds:Signature//ds:SignatureMethod",
-                                                  @namespaces)
-      algorithm = signature_method_node.attribute('Algorithm').value
-      assert_includes @allowed_signature_algorithms, algorithm
-
-
-      signature_digest_node = @metadata.at_xpath("//ds:Signature//ds:DigestMethod",
-                                                  @namespaces)
-      algorithm = signature_digest_node.attribute('Algorithm').value
-      assert_includes @allowed_digest_algorithms, algorithm
-    end
-
-    test 'conformità certificato' do
-      certificate_node = @metadata.at_xpath("//ds:X509Certificate", @namespaces)
-      certificate = certificate_node.text
-      assert certificate.present?
-      certificate_obj = OpenSSL::X509::Certificate.new Base64.decode64(certificate)
-      key_bytes_size = certificate_obj.public_key.n.num_bytes
-      assert key_bytes_size * 8 >= 1024
+      assert_response :success
     end
 
   end
