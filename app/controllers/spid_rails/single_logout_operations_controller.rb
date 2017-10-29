cd24a8a65dbb355b84b21f9bb27b0b9521126513
Jecko-o <tunedstoned@yahoo.it>
2017-10-29 18:42:01 +0100

    refactor slo controller and session params

--- app/controllers/spid_rails/single_logout_operations_controller.rb
+++ app/controllers/spid_rails/single_logout_operations_controller.rb
@@ -4,15 +4,17 @@ class SpidRails::SingleLogoutOperationsController < SpidRails::SpidController
   def new
     logout_request = SpidRails::SloRequest.new(slo_params)
     redirect_to logout_request.to_saml
-    session[:transaction_id] = logout_request.uuid
+    session[:spid_slo_id] = logout_request.uuid
   end
 
   def create
     logout_response = OneLogin::RubySaml::Logoutresponse.new(params[:SAMLResponse],
                                                              slo_settings,
-                                                             matches_request_id: session[:transaction_id])
+                                                             matches_request_id: session[:spid_slo_id])
     if logout_response.validate
+      session[:sso_params] = nil
       session[:spid_index] = nil
+      session[:spid_slo_id] = nil
       redirect_to main_app.welcome_path, notice: 'Utente correttamente sloggato'
     else
       render plain: response.inspect
@@ -22,7 +24,11 @@ class SpidRails::SingleLogoutOperationsController < SpidRails::SpidController
   private
 
   def slo_params
-    session[:sso_params].merge(session_index: session[:index])
+    {
+      host: main_app.root_url,
+      idp: session[:sso_params]['idp'],
+      session_index: session[:spid_index]
+    }
   end
 
 end
