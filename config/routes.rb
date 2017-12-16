30756f06ea7ce594d7e2584cc57e1fca31a9135e
Jecko-o <tunedstoned@yahoo.it>
2017-12-16 18:00:55 +0100

    refactor executable and routes namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  mount SpidRails::Engine, at: SpidRails.mount_point
+  mount Spid::Rails::Engine, at: Spid::Rails.mount_point
 end
 
-SpidRails::Engine.routes.draw do
+Spid::Rails::Engine.routes.draw do
   resource :metadata, only: :show,
-            path: SpidRails.metadata_path
+            path: Spid::Rails.metadata_path
   resource :sso, only: [:new, :create], controller: :single_sign_ons,
-            path: SpidRails.sso_path
+            path: Spid::Rails.sso_path
   resource :slo, only: [:new, :create], controller: :single_logout_operations,
-            path: SpidRails.slo_path
+            path: Spid::Rails.slo_path
 end
