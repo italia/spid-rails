d885ab23adb10f3bd5484dea69e3c01e2c46c074
Jecko-o <tunedstoned@yahoo.it>
2017-12-19 08:49:05 +0100

    refactor controllers tests namespace

---
+++ test/controllers/spid/rails/single_sign_ons_controller_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+module Spid
+  module Rails
+
+    class SingleSignOnsControllerTest < ActionDispatch::IntegrationTest
+      include Engine.routes.url_helpers
+
+      setup do
+        @routes = Engine.routes
+      end
+
+      # TODO: implementare vcr
+      test "get new sso" do
+        get new_sso_url(sso: { idp: :poste } )
+        assert_response :redirect
+      end
+
+      test "create sso" do
+        get new_sso_url(sso: { idp: :poste } )
+        post sso_url('SAMLResponse' => File.read('test/templates/authn_response'))
+        assert_redirected_to '/'
+      end
+
+    end
+
+  end
+end
