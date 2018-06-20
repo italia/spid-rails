6bc5c7300fae140b0a244ed2963ed5a4c242ecc0
David Librera <davidlibrera@gmail.com>
2018-06-20 14:15:21 +0200

    Metadata path is not required for issuer id

--- app/models/spid/settings.rb
+++ app/models/spid/settings.rb
@@ -51,7 +51,7 @@ module Spid
 
     def sp_attributes
       {
-        issuer: host + metadata_path,
+        issuer: host,
         assertion_consumer_service_url: host + sso_path,
         single_logout_service_url: host + slo_path,
         private_key: File.read("#{::Rails.root}/#{keys_path}/private_key.pem"),
