ee0279500d7df266a7e1663d30495751394421e7
Giacomo Bertoldi <tunedstoned@yahoo.it>
2018-06-24 16:38:54 +0200

    add generator for idp import file

---
+++ lib/generators/spid/rails/idp_importer_generator.rb
@@ -0,0 +1,21 @@
+module Spid
+  module Rails
+
+    module Generators
+
+      class IdpImporterGenerator < ::Rails::Generators::Base
+
+        source_root File.expand_path("../templates", __FILE__)
+
+        desc "Crea il file di import custom degli Idp (config/spid-rails/idp_import.yml)."
+
+        def create_import_file
+          template "idp_import.yml", "./config/spid-rails/idp_import.yml"
+        end
+
+      end
+
+    end
+
+  end
+end
