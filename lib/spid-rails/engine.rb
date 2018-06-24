9b160405eff7aa1fec5c24285fdb8adef30e7354
Giacomo Bertoldi <tunedstoned@yahoo.it>
2018-06-24 16:10:23 +0200

    load custom idp list from config/spid-rails/idp_list.yml when present

--- lib/spid-rails/engine.rb
+++ lib/spid-rails/engine.rb
@@ -6,6 +6,14 @@ module Spid
 
     class Engine < ::Rails::Engine
       isolate_namespace Spid::Rails
+
+      initializer "spid-rails.load_custom_idp_list" do
+        path_to_list = ::Rails.root.join("config", "spid-rails", "idp_list.yml")
+        if File.exist?(path_to_list)
+          Spid::Idp.import(::Rails.root.join("config", "spid-rails", "idp_list.yml"))
+        end
+      end
+
     end
 
   end
