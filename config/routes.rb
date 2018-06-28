2d2a77dc6e47222860af39e63a62117a11109ef9
Bertoldi <bertoldi.giacomo@gmail.com>
2018-06-28 16:18:16 +0200

    declare all slo routes in the same block

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   mount Spid::Rails::Engine, at: Spid::Rails.mount_point
 end
 
+Rails.application.routes.draw do
+  mount Spid::Rails::Engine, at: Spid::Rails.mount_point
+end
+
 Spid::Rails::Engine.routes.draw do
   resource :metadata, only: :show,
                       path: Spid::Rails.metadata_path
-  resource :sso, only: [:new, :create], controller: :single_sign_ons,
+  resource :sso, only: [:new, :create],
+                 controller: :single_sign_ons,
                  path: Spid::Rails.sso_path
-  resource :slo, only: [:new, :create], controller: :single_logout_operations,
-                 path: Spid::Rails.slo_path
-  get Spid::Rails.slo_path, to: 'single_logout_operations#create'
+  resource :slo, only: [:new, :create],
+                 controller: :single_logout_operations,
+                 path: Spid::Rails.slo_path do
+                   get '/', to: 'single_logout_operations#create'
+                 end
 end
