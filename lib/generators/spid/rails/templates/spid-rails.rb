5b4ddc12c7ba591f73c770fed0b59de57de2df8f
David Librera <davidlibrera@gmail.com>
2018-09-12 15:46:34 +0200

    Use new configuration class in config generator

--- lib/generators/spid/rails/templates/spid-rails.rb
+++ lib/generators/spid/rails/templates/spid-rails.rb
@@ -1,26 +1,24 @@
-Spid::Rails.tap do |config|
-  # Mount point di Spid sull'applicazione
-  # default: 'spid'
-  # config.mount_point = 'spid'
+# frozen_string_literal: true
 
-  # Url alla quale e' disponibile il metadata del provider
-  # default: 'metadata'
-  # config.metadata_path = 'metadata'
+Spid.configure do |config|
+  config.hostname = ENV.fetch('HOST')
 
-  # Url alla quale ricevere le risposte di autenticazione Saml
-  # default: 'sso'
-  # config.sso_path = 'sso'
+  config.idp_metadata_dir_path = Rails.root.join('config/idp_metadata')
+  config.private_key_pem = ENV.fetch('PRIVATE_KEY')
+  config.certificate_pem = ENV.fetch('CERTIFICATE')
 
-  # Url alla quale ricevere le risposte di logout Saml
-  # default: 'slo'
-  # config.slo_path = 'slo'
+  config.metadata_path = '/spid/metadata'
+  config.login_path = '/spid/login'
+  config.logout_path = '/spid/logout'
+  config.acs_path = '/spid/sso'
+  config.slo_path = '/spid/slo'
+  config.default_relay_state_path = '/'
 
-  # Percorso relativo alla root dell'app
-  # al quale reperire la coppia chiave privata - certificato
-  # default: 'lib/.keys'
-  # config.keys_path = 'lib/.keys/'
-
-  # Livello di crittografia SHA per la generazione delle signature
-  # default: 256
-  # config.sha = 256
+  config.digest_method = Spid::SHA512
+  config.signature_method = Spid::RSA_SHA512
+  config.acs_binding = Spid::BINDINGS_HTTP_POST
+  config.slo_binding = Spid::BINDINGS_HTTP_REDIRECT
+  config.attribute_services = [
+    { name: 'Service1', fields: ['email'] }
+  ]
 end
