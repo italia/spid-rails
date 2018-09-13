b91ac2e70c94a42c0469aafabc33c6c43ab056cf
David Librera <davidlibrera@gmail.com>
2018-09-13 17:30:37 +0200

    Helper for login and logout paths

--- lib/spid-rails/route_helper.rb
+++ lib/spid-rails/route_helper.rb
@@ -11,6 +11,44 @@ module Spid
           metadata_path
         ).to_s
       end
+
+      def spid_login_path(
+            idp_name:,
+            authn_context: nil,
+            attribute_service_index: nil
+          )
+        options = { idp_name: idp_name }
+        options[:authn_context] = authn_context if authn_context.present?
+        if attribute_service_index.present?
+          options[:attribute_service_index] = attribute_service_index
+        end
+        [
+          Spid.configuration.login_path,
+          options.to_param
+        ].join("?")
+      end
+
+      def spid_logout_path(idp_name:)
+        options = { idp_name: idp_name }
+        [
+          Spid.configuration.logout_path,
+          options.to_param
+        ].join("?")
+      end
+
+      def spid_login_url(options)
+        URI.join(
+          Spid.configuration.hostname,
+          spid_login_path(options)
+        ).to_s
+      end
+
+      def spid_logout_url(options)
+        URI.join(
+          Spid.configuration.hostname,
+          spid_logout_path(options)
+        ).to_s
+      end
     end
   end
 end
